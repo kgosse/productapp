ac6bdfd8e56270e84adf15976885ae25e633460f
Kevin Gosse <gossekevin1@gmail.com>
2015-12-13 21:07:20 +0100

    display 3 slides

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,29 @@
+require('../styles/lib/slick.css');
+require('../styles/lib/slick-theme.css');
+
 import React, { Component } from 'react';
 import Slider from 'react-slick';
 
 export default class App extends Component {
     render() {
         var settings = {
-            dots: true,
+            dots: false,
             infinite: true,
             speed: 500,
-            slidesToShow: 1,
+            slidesToShow: 3,
             slidesToScroll: 1,
-            className: 'slider_product',
+            className: 'slider__products',
             arrows: true,
-            adaptiveHeight: true
+            autoplay: true
         };
+        let products = [];
+        for (var i = 1; i <= 6; ++i)
+            products.push(<div className='slider__products__product' key={i}><h3>{i}</h3></div>)
         return (
             <div className='slider'>
                 <div className='slider__title'>Les meilleurs produits de beauté de 2015</div>
                 <Slider {...settings}>
-                    <div><h3>1</h3></div>
-                    <div><h3>2</h3></div>
-                    <div><h3>3</h3></div>
-                    <div><h3>4</h3></div>
-                    <div><h3>5</h3></div>
-                    <div><h3>6</h3></div>
+                    {products}
                 </Slider>
             </div>
         );
